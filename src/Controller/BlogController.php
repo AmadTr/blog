cc888e1c4ae32c56f143afa4dd7751713f881526
Amad <amadou.traoredev@gmail.com>
2022-03-20 20:28:28 +0100

    article et contact

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,19 +2,46 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use App\Entity\Article;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ArticleRepository; //crt alt i
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/blog", name="app_blog")
+     * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(ArticleRepository $artRepo): Response
     {
+        $articles = $artRepo->findAll();
+        // dd($articles); // dump and die
+
         return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
+            'articles'=> $articles
+        ]);
+    }
+
+
+    /**
+     * @Route("blog/contact", name="contact")
+     */
+    public function contact(): Response
+    {
+        return $this->render('blog/contact.html.twig');
+    }
+
+
+
+    /**
+     * @Route("/article/{id}", name="art-detail")
+     */
+    public function artDetail(Article $article): Response{
+
+        return $this->render('blog/artDetail.html.twig', [
+            'article'=>$article
         ]);
     }
 }
